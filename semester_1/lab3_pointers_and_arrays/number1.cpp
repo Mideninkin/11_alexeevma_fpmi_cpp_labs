48e316018efca35ed2c12959eafd263038a91713
Алексеев Михаил Андреевич <mishka.alexeev@gmail.com>
2025-10-12 21:15:10 +0300

    lab3number1.cpp

--- semester_1/lab3_pointers_and_arrays/number1.cpp
+++ semester_1/lab3_pointers_and_arrays/number1.cpp
@@ -1,85 +1,54 @@
 #include <iostream>
 #include <random>
+#include <ctime>
 
-const int max = 100;
-int main()
+void input (int* x, int min, int max)
 {
-    int n;
-    int arr[max];
-
-    std::cout << "Enter number of array elements (1-" << max << "):\n";
-
-    if (!(std::cin >> n) || (n < 1) || (n > max))
+    int d;
+    while (!(std::cin >> d) || (d < min) || (d > max))
     {
-        std::cout << "Invalid input. Try again";
-        return 1;
+        std::cout << "Invalid input" << '\n';
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
+    *x = d;
+}
 
-    char answer;
-    std::cout << "What input do you choose: 'M' or 'A'? ";
-    std::cin >> answer;
+void Swap( int *q, int *o)
+{
+    int temp = *q;
+    *q = *o;
+    *o = temp;
+}
 
-    if (answer == 'M' || answer == 'm')
+void OutputOfArray(int* arr, int n)
     {
-        std::cout << "Enter " << n << " integer numbers:\n";
-        for (int i = 0; i < n; i++)
-        {
-            std::cout << "Element [" << i << "]: ";
-            std::cin >> arr[i];
-        }
-    }
-    else if (answer == 'A' || answer == 'a')
-    {
-        int a, b;
-        std::cout << "Enter interval borders [a, b]:\n";
-        std::cout << "a = ";
-        if (!(std::cin >> a));
-        std::cout << "b = ";
-        if (!(std::cin >> b));
-
-        if (a > b)
-        {
-            int temp = a;
-            a = b;
-            b = temp;
-        }
-        std::mt19937 gen(45218965);
-        std::uniform_int_distribution<int> dist(a, b);
-        int x = dist(gen);
-
-        for (int i = 0; i < n; i++)
-        {
-            arr[i] = dist(gen);
-        }
-
-        std::cout << "Array was filled with random numbers from [" << a << ", " << b << "]\n";
-    }
-    else
-    {
-        std::cout << "Invalid input\n";
-        return 1;
-    }
-
     std::cout << "Array: [";
     for (int i = 0; i < n; i++)
     {
-        std::cout << arr[i];
+        std::cout << *(arr + i);
         if (i < n - 1)
             std::cout << ", ";
     }
     std::cout << "]\n";
-
-    int mult = 1;
-    for (int i = 1; i < n; i += 2)
-    {
-        mult *= arr[i];
     }
-    std::cout << "Product of even-numbered elements: " << mult << "\n";
 
+int Multiplicate (int* arr, int n)
+    {
+        int mult = 1;
+    for (int i = 1; i < n; i += 2)
+        {
+            mult *= *(arr + i);
+        }
+    return mult;
+    }    
+
+int SumBetweenNilElements(int* arr, int n)
+    {
     int firstNil = -1, lastNil = -1;
     for (int i = 0; i < n; i++)
     {
-        if (arr[i] == 0)
+        if (*(arr + i) == 0)
         {
             firstNil = i;
             break;
@@ -87,35 +56,115 @@ int main()
     }
     for (int i = n - 1; i >= 0; i--)
     {
-        if (arr[i] == 0)
+        if (*(arr + i) == 0)
         {
             lastNil = i;
             break;
         }
     }
     int sum = 0;
-    if ((firstNil != lastNil) && (firstNil != -1) && (lastNil != -1))
+    if (firstNil != lastNil && firstNil != -1 && lastNil != -1)
     {
         for (int i = firstNil + 1; i <= lastNil - 1; i++)
         {
-            sum += arr[i];
+            sum += *(arr + i);
         }
     }
+    return sum;
+}
+
+void NewArray (int* arr, int n)
+    {
+        for (int i = 0; i < n - 1; i++)
+        {
+            if ((*(arr + i) > 0) && (*(arr + i + 1) < 0))
+            {
+                Swap(arr + 1, arr + i + 1);
+            }
+        }
+    }
+int main()
+{
+ const int max = 100;
+    int n;
+    int arr[max];
+
+    std::cout << "Enter number of array elements (1-" << max << "):\n";
+
+    input (&n, 1, 100);
+
+    std::cout << "Number of elements: " << n << std::endl;
+
+    char answer;
+    std::cout << "What input do you choose: 'M' or 'A'? ";
+    std::cin >> answer;
+
+    int a, b;
+    switch (answer)
+    {
+    case 'M':
+    case 'm':
+    {
+        std::cout << "Enter " << n << " integer numbers:\n";
+        for (int i = 0; i < n; i++)
+        {
+            std::cout << "Element [" << i << "]: ";
+            std::cin >> arr[i];
+        }
+        break;
+    }
+    case 'A':
+    case 'a':
+    {    std::cout << "Enter interval borders [a, b]:\n";
+        
+        std::cout << "a = ";
+    while (!(std::cin >> a))
+    {
+        std::cout << "Invalid input for a. Please enter an integer: ";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+    
+    std::cout << "b = ";
+    while (!(std::cin >> b))
+    {
+        std::cout << "Invalid input for b. Please enter an integer: ";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+        if (a > b)
+        {
+            Swap(&a, &b);
+        }
+        std::srand(std::time(0));
+        for (int i = 0; i < n; i++)
+        {
+            int random_number = a + std::rand() % (b - a + 1);
+            arr[i] = random_number;
+        }
+
+        std::cout << "Array was filled with random numbers from [" << a << ", " << b << "]\n";
+        break;
+    }
+    default:
+        std::cout << "Invalid input\n";
+        return 1;
+    }
+    
+    OutputOfArray(arr, n);
+
+    int mult = Multiplicate(arr, n);
+    std::cout << "Product of even numbered elements is: " << mult << std::endl;
+    
+    int sum = SumBetweenNilElements(arr, n);
     std::cout << "Sum between first and last nil elements is: " << sum << std::endl;
 
-    for (int i = 0; i < n - 1; i++)
-    {
-        if ((arr[i] > 0) && (arr[i + 1] < 0))
-        {
-            int k = arr[i];
-            arr[i] = arr[i + 1];
-            arr[i + 1] = k;
-        }
-    }
-    std::cout << "Modifided array: [";
+    NewArray(arr, n);
+
+    std::cout << "Modifieded array: [";
     for (int i = 0; i < n; i++)
     {
-        std::cout << arr[i];
+        std::cout << *(arr + i);
         if (i < n - 1)
             std::cout << ", ";
     }
@@ -123,3 +172,4 @@ int main()
 
     return 0;
 }
+
