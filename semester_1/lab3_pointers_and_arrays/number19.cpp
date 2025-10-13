54e4d411a2b940f82f686d792797e3228e5b483b
Алексеев Михаил Андреевич <mishka.alexeev@gmail.com>
2025-10-13 20:45:04 +0300

    lab3number19.cpp

--- semester_1/lab3_pointers_and_arrays/number19.cpp
+++ semester_1/lab3_pointers_and_arrays/number19.cpp
@@ -1,75 +1,41 @@
 #include <iostream>
 #include <random>
+#include <cmath>
 
-const int max = 100;
-int main()
+void input (int* x, int min, int max)
 {
-    int n;
-    std::cout << "Enter number of array elements (1-" << max << "):\n";
-
-    if (!(std::cin >> n) || (n < 1) || (n > max))
+    int d;
+    while (!(std::cin >> d) || (d < min) || (d > max))
     {
-        std::cout << "Invalid input. Try again";
-        return 1;
+        std::cout << "Invalid input" << '\n';
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
+    *x = d;
+}
 
-    double *arr = new double[max];
+void Swap( double *q, double *o)
+{
+    double temp = *q;
+    *q = *o;
+    *o = temp;
+}
 
-    char answer;
-    std::cout << "What input do you choose: 'M' or 'A'? ";
-    std::cin >> answer;
-
-    if (answer == 'M' || answer == 'm')
+void OutputOfArray(double* arr, int n)
     {
-        std::cout << "Enter " << n << " real numbers:\n";
-        for (int i = 0; i < n; i++)
-        {
-            std::cout << "Element [" << i << "]: ";
-            std::cin >> arr[i];
-        }
-    }
-    else if (answer == 'A' || answer == 'a')
-    {
-        double a, b;
-        std::cout << "Enter interval borders [a, b]:\n";
-        std::cout << "a = ";
-        if (!(std::cin >> a));
-        std::cout << "b = ";
-        if (!(std::cin >> b));
-
-        if (a > b)
-        {
-            double temp = a;
-            a = b;
-            b = temp;
-        }
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_real_distribution<double> dist(a, b);
-        for (int i = 0; i < n; i++)
-        {
-            arr[i] = dist(gen);
-        }
-
-        std::cout << "Array was filled with random numbers from [" << a << ", " << b << "]\n";
-    }
-    else
-    {
-        std::cout << "Invalid input\n";
-        delete[] arr;
-        return 1;
-    }
-
     std::cout << "Array: [";
     for (int i = 0; i < n; i++)
     {
-        std::cout << arr[i];
+        std::cout << *(arr + i);
         if (i < n - 1)
             std::cout << ", ";
     }
     std::cout << "]\n";
+    }
 
-    int count = 0;
+int Count (double* arr, int n)
+    {
+        int counter = 0;
 
     for (int i = 0; i < n; i++)
     {
@@ -77,7 +43,7 @@ int main()
 
         for (int k = 0; k < i; k++)
         {
-            if (arr[i] == arr[k])
+            if (*(arr + i) == *(arr + k))
             {
                 unique = false;
                 break;
@@ -85,15 +51,52 @@ int main()
         }
         if (unique)
         {
-            count++;
+            counter++;
         }
     }
-    std::cout << "Amount of different elements is: " << count << std::endl;
+        return counter;
+    }
 
-    double firstNegative = -1, lastNegative = -1;
+int MakeArray (double* arr, int n, double x)
+    {
+        int newArray = 0;
+
     for (int i = 0; i < n; i++)
     {
-        if (arr[i] < 0)
+        double abs_num = *(arr + i);
+        if (abs_num < 0)
+        {
+            abs_num = -abs_num;
+        }
+        if (abs_num > x)
+        {
+            *(arr + newArray) = *(arr + i);
+            newArray++;
+        }
+    }
+    for (int i = newArray; i < n; i++)
+    {
+        *(arr + i) = 0.0;
+    }
+    std::cout << "New array (elements with |value| > " << x << "): [";
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << *(arr + i);
+        if (i < n - 1)
+        {
+            std::cout << ", ";
+        }
+    }
+    std::cout << "]\n";
+    return newArray;
+    }
+
+double SumBetweenNegatives(double* arr, int n)
+    {
+    int firstNegative = -1, lastNegative = -1;
+    for (int i = 0; i < n; i++)
+    {
+        if (*(arr + i) < 0)
         {
             firstNegative = i;
             break;
@@ -101,7 +104,7 @@ int main()
     }
     for (int i = n - 1; i >= 0; i--)
     {
-        if (arr[i] < 0)
+        if (*(arr + i) < 0)
         {
             lastNegative = i;
             break;
@@ -112,48 +115,110 @@ int main()
     {
         for (int i = firstNegative + 1; i <= lastNegative - 1; i++)
         {
-            sum += arr[i];
+            sum += *(arr + i);
         }
     }
+    return sum;
+    }  
+    
+int main()
+{
+    const int max = 100;
+    int n;
+
+    std::cout << "Enter number of array elements (1-" << max << "):\n";
+
+    input (&n, 1, 100);
+
+    std::cout << "Number of elements: " << n << std::endl;
+
+    char answer;
+    std::cout << "What input do you choose: 'M' or 'A'? ";
+    std::cin >> answer;
+
+    double* arr = new double[max];
+
+    double a, b;
+    switch (answer)
+    {
+    case 'M':
+    case 'm':
+    {
+        std::cout << "Enter " << n << " real numbers:\n";
+        for (int i = 0; i < n; i++)
+        {
+            std::cout << "Element [" << i << "]: ";
+            std::cin >> *(arr + i);
+        }
+        break;
+    }
+    case 'A':
+    case 'a':
+    {    std::cout << "Enter interval borders [a, b]:\n";
+        
+        std::cout << "a = ";
+    while (!(std::cin >> a))
+    {
+        std::cout << "Invalid input for a. Please enter a number: ";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+    
+    std::cout << "b = ";
+    while (!(std::cin >> b))
+    {
+        std::cout << "Invalid input for b. Please enter a number: ";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+        if (a > b)
+        {
+            Swap(&a, &b);
+        }
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_real_distribution<> dist(a, b);
+
+        for (int i = 0; i < n; i++)
+        {
+            *(arr + i) = dist(gen);
+        }
+
+        std::cout << "Array was filled with random numbers from [" << a << ", " << b << "]\n";
+        break;
+    }
+    default:
+        std::cout << "Invalid input\n";
+        delete [] arr;
+        return 1;
+    }
+    
+    OutputOfArray(arr, n);
+
+    int counter = Count(arr, n);
+
+    std::cout << "Amount of different elements is: " << counter << std::endl;
+
+    double sum = SumBetweenNegatives(arr, n);
+
     std::cout << "Sum between first and last negative elements is: " << sum << std::endl;
 
     double x;
     std::cout << "Enter number X: ";
-    std::cin >> x;
+    while (!(std::cin >> x))
+    {
+        std::cout << "Invalid input" << '\n';
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
 
-    int newSize = 0;
-    for (int i = 0; i < n; i++)
-    {
-        double abs_num = arr[i];
-        if (abs_num < 0)
-        {
-            abs_num = -abs_num;
-        }
-        if (abs_num > x)
-        {
-            arr[newSize] = arr[i];
-            newSize++;
-        }
-    }
-    for (int i = newSize; i < n; i++)
-    {
-        arr[i] = 0.0;
-    }
-    std::cout << "Compressed array (elements with |value| > " << x << "): [";
-    for (int i = 0; i < n; i++)
-    {
-        std::cout << arr[i];
-        if (i < n - 1)
-        {
-            std::cout << ", ";
-        }
-    }
-    std::cout << "]\n";
-    std::cout << "Number of remaining elements: " << newSize << std::endl;
-    std::cout << "Number of removed elements: " << n - newSize << std::endl;
+    int newArray = MakeArray(arr, n, x);
+    
+    std::cout << "Number of remaining elements: " << newArray << std::endl;
+    std::cout << "Number of removed elements: " << n - newArray << std::endl;
 
     delete[] arr;
-
+    
     return 0;
 }
 
