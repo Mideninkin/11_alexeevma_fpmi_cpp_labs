45bbf04f31ee475b85d975f23c1c9b979721c0e5
Алексеев Михаил Андреевич <mishka.alexeev@gmail.com>
2025-10-14 10:27:14 +0300

    sort.cpp

---
+++ semester_1/sort.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+void SortBubble (int[a], int size)
+    {
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0 ; j < n - i - 1; j++)
+        {
+        if (abs(a[j]) > abs(a[j + 1]))
+        void Swap (int *a[i], int *a[j])
+        {
+            m = a[i];
+            a[i] = a[j];
+            a[j] = m;
+        }
+        } 
+    }
+    }
+
+main()
+{ 
+    SortBubble ()
+
+    return 0;
+}
+  
