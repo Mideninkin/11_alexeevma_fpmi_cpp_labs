f6daa82e3394ed8a90204f29e1d1ceae7a8b8123
Алексеев Михаил Андреевич <mishka.alexeev@gmail.com>
2025-12-15 21:18:01 +0300

    maybe will do more here

--- semester_1/rgr1_forward_list/forward_list/forward_list_impl.cpp
+++ semester_1/rgr1_forward_list/forward_list/forward_list_impl.cpp
@@ -1,3 +1,196 @@
 #include "forward_list_impl.h"
 
-// your code goes here
+// default constructor
+ForwardList::ForwardList() : front_{nullptr}, size_{0} 
+{}
+
+// copy constructor
+ForwardList::ForwardList(const ForwardList& rhs) : front_{nullptr}, size_{0} 
+{
+    Node* current = rhs.front_;
+    Node* prev = nullptr;
+    while(current) 
+    {
+        Node* other_node = new Node(current->value_);
+        if (front_) 
+        {
+            prev->next_ = other_node;
+        }
+        else 
+        {
+            front_ = other_node;
+        }
+        current = current->next_;
+        prev = other_node;
+        ++size_;
+    }
+}
+
+// Constructs a ForwardList with count copies of elements with value value.
+ForwardList::ForwardList(size_t count, int32_t value) : front_{nullptr}, size_{0} 
+{
+    for (size_t i = 0; i < count; i++) 
+    {
+        PushFront(value);
+    }
+}
+
+// Constructs a ForwardList with std::initializer_list<int32_t>
+ForwardList::ForwardList(std::initializer_list<int32_t> init) :  front_{nullptr}, size_{0}
+{
+    if (init.size() == 0) return;
+    
+    for (auto it = std::rbegin(init); it != std::rend(init); ++it) 
+    {
+        PushFront(*it);
+    }
+}
+
+// operator= overloading
+ForwardList& ForwardList::operator=(const ForwardList& rhs) 
+{
+    if (this == &rhs) 
+    {
+        return *this;
+    }
+    
+    Clear();
+    Node* current = rhs.front_;
+    Node* prev = nullptr;
+    while(current) 
+    {
+        Node* new_node = new Node(current->value_);
+        if (front_) 
+        {
+            prev->next_ = new_node;
+        }
+        else 
+        {
+            front_ = new_node;
+        }
+        current = current->next_;
+        prev = new_node;
+        ++size_;
+    }
+    return *this;
+}
+
+// destructor
+ForwardList::~ForwardList() 
+{
+    Clear();
+}
+
+// insert new element on the top of the list
+void ForwardList::PushFront(int32_t value) 
+{
+    Node* new_node = new Node(value);
+    new_node->next_ = front_;
+    front_ = new_node;
+    size_++;
+}
+
+// delete first element of the list
+void ForwardList::PopFront() 
+{
+    if (front_ == nullptr) 
+    {
+        throw std::runtime_error("Error! List is empty!");
+    }
+    Node* new_front = front_->next_;
+    delete front_;
+    front_ = new_front;
+    size_--;
+}
+
+// remove every occurence of an element in the list
+// whose value equals to param value,
+// the remaining elements must remain in the same order
+void ForwardList::Remove(int32_t value) 
+{
+    while (front_ && front_->value_ == value) 
+    {
+        PopFront();
+    }
+    if (!front_) 
+    {
+        return;
+    }
+    Node* prev = front_;
+    Node* current = prev->next_;
+    while (current) 
+    {
+        if (current->value_ == value) 
+        {
+            --size_;
+            prev->next_ = current->next_;
+            delete current;
+            current = prev->next_; 
+        }
+        else 
+        {
+            prev = current;
+            current = current->next_;
+        }
+    }
+}
+
+// erases all the elements
+void ForwardList::Clear() 
+{
+    while(front_) 
+    {
+        PopFront();
+    }
+}
+
+// find list's element by the value
+// returns true, if element exists
+// otherwise, returns false
+bool ForwardList::FindByValue(int32_t value) 
+{
+    Node* current = front_;
+    while (current) 
+    {
+        if (current->value_ == value) 
+        {
+            return true;
+        }
+        current = current->next_;
+    }
+    return false;
+}
+
+// print list's elements to stream separated by space
+void ForwardList::Print(std::ostream& out) 
+{
+    Node* current = front_;
+    while(current) 
+    {
+        if (current == front_) 
+        {
+            out << current->value_;
+        }
+        else 
+        {
+            out << " " << current->value_;
+        }
+        current = current->next_;
+    }
+}
+
+// get first element of the list
+int32_t ForwardList::Front() const 
+{
+    if (front_ == nullptr) 
+    {
+        throw std::runtime_error("Error! List is empty!");
+    }
+    return front_->value_;
+}
+
+// get size of the list
+size_t ForwardList::Size() const 
+{
+    return size_;
+}
