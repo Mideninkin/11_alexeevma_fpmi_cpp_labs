1f7a2bdbd016f8c08437ea35999454fd8e4d0848
Mihasik <mishka.alexeev@gmail.com>
2026-02-08 11:50:19 +0300

    Коммит

--- semester_1/rgr2_vector_stl/part1/main.cpp
+++ semester_1/rgr2_vector_stl/part1/main.cpp
@@ -1,9 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <limits>
 
-
-
+void InputOfNumbersInVector (std::vector<int>& vector) {
+int number;
+std::cout << "Enter integer numbers: ";
+while (std::cin >> number) {
+vector.push_back(number);
+if (!std::cin >> number) {       
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+}
+}
 int main() {
+std::vector<int> vec;
+InputOfNumbersInVector (vec);
 
-    // your code goes here
-
-    return 0;
+return 0;
 }
